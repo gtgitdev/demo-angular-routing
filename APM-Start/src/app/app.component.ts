aad4911bee32755cefff9ed0efa2946ace986f0f
gtgitdev <g.turton@hotmail.com>
2019-03-08 05:01:37 -0500

    secondary routs, chapter 10 complete

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router, Event, NavigationEnd, NavigationStart, NavigationCancel, Naviga
 
 import { AuthService } from './user/auth.service';
 import { slideInAnimation } from './app.animation';
+import { MessageService } from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -25,23 +26,31 @@ export class AppComponent {
     return '';
   }
 
+  get isMessageDisplayed(): boolean {
+    if (this.messageService) {
+      return this.messageService.isDisplayed;
+    }
+
+  }
+
   constructor(private authService: AuthService,
-    private router: Router) {
+    private router: Router,
+    private messageService: MessageService) {
     router.events.subscribe((routerEvent: Event) => {
       this.checkRouterEvent(routerEvent);
     });
   }
 
-  checkRouterEvent(routerEvent: Event){
-    if( routerEvent instanceof NavigationStart) {
+  checkRouterEvent(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
       this.loading = true;
     }
 
     if (routerEvent instanceof NavigationCancel ||
-       routerEvent instanceof NavigationEnd || 
-       routerEvent instanceof NavigationError) {
-         this.loading = false;
-       }
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
   }
 
   logOut(): void {
@@ -49,5 +58,15 @@ export class AppComponent {
     console.log('Log out');
     this.router.navigateByUrl('/welcome');
 
+  }
+
+  displayMessages(): void {
+    this.router.navigate([{ outlets: { popup: ['messages'] } }]);
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this.router.navigate([{ outlets: { popup: null } }]);
+    this.messageService.isDisplayed = false;
   }
 }
